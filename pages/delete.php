289fa853c1ed598932e0fef17036f20f8ad786dd
ogu10 <t_ogura+4@jobins.jp>
2022-04-26 14:01:40 +0900

    save 20220426_14:01

--- pages/delete.php
+++ pages/delete.php
@@ -14,6 +14,7 @@ try {
 }
 
 ?>
+
 <!--
 <!DOCTYPE html>
 <html>
--- players_list4.php
+++ players_list4.php
@@ -15,59 +15,61 @@
     <div align='center'>
         <font color=red>
 
-            <?php
-            include 'pages/connection.php';
+        <?php
+        include 'pages/connection.php';
 
-            $sortBy = isset($_GET["column"])? $_GET["column"] : "id";
-            $sortOrder = isset($_GET["sort"])? $_GET["sort"] : "DESC";
-            $searchName = isset($_GET["search_word"])? $_GET["search_word"] : '';
-            $searchTeam = isset($_GET["team_belongings"])? $_GET["team_belongings"] : [];
-            $elements = is_array($searchTeam)? count($searchTeam): '0';
-
-
-            $array = '';
-            $y = 1;
-            foreach ($searchTeam as $teams):
-            $array .= "'".$teams."'";
-            if($y < $elements){
-                $array .= ", ";}
-            $y ++;
-            endforeach;
+        $sortBy = isset($_GET["column"])? $_GET["column"] : "id";
+        $sortOrder = isset($_GET["sort"])? $_GET["sort"] : "DESC";
+        $searchName = isset($_GET["search_word"])? $_GET["search_word"] : '';
+        $searchTeam = isset($_GET["team_belongings"])? $_GET["team_belongings"] : [];
+        $elements = is_array($searchTeam)? count($searchTeam): '0';
+        $deleteID = isset($_GET["deleteID"])? $_GET["deleteID"]: '';
 
 
-            if($array == ''){
-            $query =  "SELECT * FROM players WHERE `name` LIKE '%$searchName%' ORDER BY $sortBy $sortOrder, Length(team)";
-            }else{
-            $query =  "SELECT * FROM players WHERE `name` LIKE '%$searchName%' AND `team` IN ($array) ORDER BY $sortBy $sortOrder, Length(team)";}
-            /*var_dump($query);*/
+        $array = '';
+        $y = 1;
+        foreach ($searchTeam as $teams):
+        $array .= "'".$teams."'";
+        if($y < $elements){
+            $array .= ", ";}
+        $y ++;
+        endforeach;
 
-            $result = 0;
-            $stmt = $dbh->query($query);
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            echo "test";
-            print_r(sizeof($result));
-            $result_t = 0;
-            $teams = $dbh->query('SELECT DISTINCT `team` FROM players ORDER BY Length(`team`) DESC LIMIT 15');
-            $result_t = $teams->fetchAll(PDO::FETCH_ASSOC);
-            $id_max = intval($dbh->query("SELECT max(id) FROM players")->fetchColumn());
-            ?>
 
-            <form action="players_list4.php" method="GET">
-                <input type="text" id="search_word" name="search_word" placeholder="search name"  value="<?php echo $searchName ?>">
-                <input type="button" value="clear" onclick="clearName()" /><br><br><b>
-                    <?php $x=1; foreach ($result_t as $value_t): ?>
-                        <input type="checkbox" name="team_belongings[]" value="<?php echo $value_t['team'] ?>" id="team<?php echo $x ?>"
-                            <?php if(is_array($searchTeam)){if(in_array($value_t['team'], $searchTeam)){echo "checked";}} ?>>
-                        <?php echo $value_t['team'] ?><?php if($x % 5 ==0){echo "<br>";}?><?php $x++ ?>
-                    <?php endforeach ?><br><br></b>
-                <input type="hidden" name="sort" id="searchSort" value="<?php echo $sortOrder ?>">
-                <input type="hidden" name="column" id="searchColumn" value="<?php echo $sortBy ?>">
-                <button type="submit" id="submitButton" class="button3">
-                    <i class="fa-regular fa-futbol"></i> Search it！</button><br>
+        $del = $dbh->prepare('DELETE FROM players WHERE id = :id');
+        $del->execute(array(':id' => $deleteID));
+        echo $deleteID;
+        if($array == ''){
+        $query =  "SELECT * FROM players WHERE `name` LIKE '%$searchName%' ORDER BY $sortBy $sortOrder, Length(team)";
+        }else{
+        $query =  "SELECT * FROM players WHERE `name` LIKE '%$searchName%' AND `team` IN ($array) ORDER BY $sortBy $sortOrder, Length(team)";}
+        /*var_dump($query);*/
 
-                <table class="players=country">
+        $result = 0;
+        $stmt = $dbh->query($query);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $result_t = 0;
+        $teams = $dbh->query('SELECT DISTINCT `team` FROM players ORDER BY Length(`team`) DESC LIMIT 15');
+        $result_t = $teams->fetchAll(PDO::FETCH_ASSOC);
+        $id_max = intval($dbh->query("SELECT max(id) FROM players")->fetchColumn());
+        ?>
+
+        <form name="form1" id="form1" action="players_list4.php" method="GET">
+            <input type="text" id="search_word" name="search_word" placeholder="search name"  value="<?php echo $searchName ?>">
+            <input type="button" value="clear" onclick="clearName()" /><br><br><b>
+                <?php $x=1; foreach ($result_t as $value_t): ?>
+                    <input type="checkbox" name="team_belongings[]" value="<?php echo $value_t['team'] ?>" id="team<?php echo $x ?>"
+                        <?php if(is_array($searchTeam)){if(in_array($value_t['team'], $searchTeam)){echo "checked";}} ?>>
+                    <?php echo $value_t['team'] ?><?php if($x % 5 ==0){echo "<br>";}?><?php $x++ ?>
+                <?php endforeach ?><br><br></b>
+            <input type="hidden" name="sort" id="searchSort" value="<?php echo $sortOrder ?>">
+            <input type="hidden" name="column" id="searchColumn" value="<?php echo $sortBy ?>">
+            <input type="text" name="deleteID" id="deleteID" value="<?php echo $deleteID ?>">
+            <button type="submit" id="submitButton" class="button3">
+                <i class="fa-regular fa-futbol"></i> Search it！</button><br>
+
+            <table class="players=country">
     </div>
-    <tr>
         <th>No. <button class='button5' type="submit" onclick="sortFunction('No')"><i class="fa-solid fa-bars"></i></button>
         <th>name <button class='button5' type="submit" onclick="sortFunction('name')"><i class="fa-solid fa-bars"></i></button>
         <th>team <button class='button5' type="submit" onclick="sortFunction('Length(team)')"><i class="fa-solid fa-bars"></i></button>
@@ -89,7 +91,7 @@
                 <i class="fa-solid fa-pen-nib"></i>
                 <?php echo "</a>\n";
                 echo "<td>";
-                echo "<button class='button3' id='deleteButton' value='${value["id"]}' onclick='deleteFunc(this)' >";
+                echo "<button class='button3' id='deleteButton' value='${value["id"]}' onclick='deleteFunc(this)'>";
 /*                echo $value["id"];*/?>
                 <i class="fa-solid fa-trash"></i>
                 <?php echo "</button>\n";
@@ -164,11 +166,13 @@ if(!isset($_SESSION["user_name"])) {
     function deleteFunc(target){
         var target_value = target.value;
         var answer = window.confirm('Delete it...?');
-        /*alert('you clicked button-id: ' + target_value);*/
-        if(answer) {
-            window.location.href= 'pages/delete.php?id=' + target_value;
-            window.reload();
-        }else {
+        alert('you clicked button-id: ' + target_value);
+        if(answer){
+            document.getElementById("deleteID").innerHTML = target_value;
+/*            window.location.href= 'pages/delete.php?id=' + target_value;
+            window.reload();*/
+            document.form1.submit();
+        }else{
         }}
 
 /*
