5e2e3c389b8ae3fcdb33c4bbf5171c7a752b46bb
ogu10 <t_ogura+4@jobins.jp>
2022-05-09 10:29:12 +0900

    save 20220509_10:29

--- test5/header.php
+++ test5/header.php
@@ -1,1 +1,6 @@
-<?php
+<br><br><br>
+<!--<link rel="stylesheet" href="/web_test3/pages/test4/test5.css">
+<link rel="stylesheet" href="/web_test3/pages/test4/test4.css">
+<link rel="stylesheet" href="/web_test3/pages/test4/Mr_button.css">
+<body id="background" background="../images/4_9.jpg" alt="objectfit"></body>
+-->
