0458c607c7876e3649666b60d2d7ded9e7dcc292
ogu10 <t_ogura+4@jobins.jp>
2022-05-20 09:42:53 +0900

    save 20220520_09:42

--- pages/test9/20220516/test12.php
+++ pages/test9/20220516/test12.php
@@ -1,6 +1,22 @@
+
 <link href="test12.css" rel="stylesheet">
 
+<h1>CSSでツールチップ（吹き出し）</h1>
+<br>
+<br>
+
+例1
+<span class="tooltip">
+  <span class="tooltip-text">サンプル</span>
+  Example
+</span>
+
+<br>
+<br>
+<br>
+
+例2
 <button class="tooltip">
-    <span class="tooltip-text">ツールチップの内容</span>
-    ボタンテキスト
+    ✏️
+    <span class="tooltip-text">編集する</span>
 </button>
